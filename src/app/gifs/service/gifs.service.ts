e2e05a62250dda0e1068035dc0a8ac91f0de22dc
Adolfo Ajucum <martin_aj22@hotmail.com>
2023-02-23 15:27:22 -0600

    realizar peticion

--- src/app/gifs/service/gifs.service.ts
+++ src/app/gifs/service/gifs.service.ts
@@ -5,6 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
 
+  private apiKey: string = 'p3xdPHblgT9EcERTfO0MzLaIDBHnzB26';
   private _historial: string[] = [];
 
   get historial (){
@@ -12,20 +13,20 @@ export class GifsService {
   }
 
 
-  buscarGifs( query: string = ''){
+  async buscarGifs( query: string = ''){
     
     query = query.trim().toLocaleLowerCase();
 
     if(!this._historial.includes(query)){
 
       this._historial.unshift( query);
-    
+      this._historial = this._historial.splice(0,10);
+
     }
 
-    this._historial = this._historial.splice(0,10);
-
-    console.log(this._historial);
+    const resp = await fetch('http://api.giphy.com/v1/gifs/search?api_key=p3xdPHblgT9EcERTfO0MzLaIDBHnzB26&q=Dragon ball')
+    const data = await resp.json();
+    console.log(data);
   }
-
 
 }

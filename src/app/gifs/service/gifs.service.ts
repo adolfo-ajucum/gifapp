12e08f8362c184c4a892a1936018178d655cfd2a
Adolfo Ajucum <martin_aj22@hotmail.com>
2023-02-24 12:37:37 -0600

    obtener imagenes desde el sidebar

--- src/app/gifs/service/gifs.service.ts
+++ src/app/gifs/service/gifs.service.ts
@@ -17,7 +17,15 @@ export class GifsService {
     return [...this._historial];
   }
 
-  constructor( private http: HttpClient) {}
+  constructor( private http: HttpClient) {
+
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
+
+    // if(localStorage.getItem('historial')){
+    //   this._historial = JSON.parse(localStorage.getItem('historial')!);
+    // }
+  }
 
  buscarGifs( query: string = ''){
     
@@ -28,13 +36,16 @@ export class GifsService {
       this._historial.unshift( query);
       this._historial = this._historial.splice(0,10);
 
+      localStorage.setItem('historial', JSON.stringify(this._historial))
+      
+
     }
 
     this.http.get<SeachGifsResponde>(`https://api.giphy.com/v1/gifs/search?api_key=p3xdPHblgT9EcERTfO0MzLaIDBHnzB26&q=${query}&limit=10`)
     .subscribe( (response) => {
       console.log(response.data);
       this.resultados = response.data;
-      response.data[0].images.downsized_medium.url;
+      localStorage.setItem('resultados', JSON.stringify(this.resultados))
     } )
 
   }
